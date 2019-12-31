72a16d590779971c1acce341b58e461d33d77a8c
KEZKA <world.is.unpredictable@gmail.com>
2019-12-31 16:43:14 +0500

    super commit 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QTableWidgetItem, QApplication, QMainWindow
+from PyQt5.QtWidgets import QTableWidgetItem, QApplication, QMainWindow, QWidget
 
 
 class COFFEE(QMainWindow):
@@ -12,6 +12,7 @@ class COFFEE(QMainWindow):
         self.run()
 
     def run(self):
+        self.table.setColumnCount(0)
         con = sqlite3.connect('coffee.sqlite')
         cur = con.cursor()
         result = cur.execute("""SELECT * FROM coffee""").fetchall()
@@ -32,8 +33,48 @@ class COFFEE(QMainWindow):
         con.close()
 
 
+class COFFEE_add(QWidget):
+    def __init__(self, wnd):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.wnd = wnd
+        self.pb.clicked.connect(self.run)
+
+    def run(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM coffee""").fetchall()
+        print(result)
+        ID = self.id.value()
+        sort = self.sort.text()
+        step = self.step.value()
+        type_coffee = self.type.text()
+        description = self.description.text()
+        price = self.price.value()
+        volume = self.volume.value()
+        a = (ID, sort, step, type_coffee, description, price, volume)
+        b = ('ID', 'variety', 'roasting_degree', 'type', 'description', 'price', 'volume_l')
+        if ID == 0:
+            cur.execute("""INSERT INTO coffee(variety, roasting_degree, type, description, price, volume_l) VALUES"""
+                        + str(a[1:]))
+        elif ID in map(lambda x: x[0], result):
+            que = 'UPDATE coffee SET '
+            for i in range(1, len(a[1:])):
+                que += "{} = '{}',\n".format(b[i], a[i])
+            cur.execute(que[:-2] + """ WHERE ID = """ + str(a[0]))
+        else:
+            cur.execute(
+                """INSERT INTO coffee(ID, variety, roasting_degree, type, description, price, volume_l) VALUES"""
+                + str(a))
+        con.commit()
+        self.wnd.run()
+        con.close()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     wnd = COFFEE()
+    wnd2 = COFFEE_add(wnd)
+    wnd2.show()
     wnd.show()
     sys.exit(app.exec())
